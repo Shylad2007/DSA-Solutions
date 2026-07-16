caa1313aa9fc075c6d56ef5566a42abd4021d665
Piyush Sohanda <psohanda007@gmail.com>
2026-07-16 08:10:25 +0530

    Time: 1 ms (100%), Space: 43.7 MB (76.31%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,50 +1,44 @@
 class Solution {
     public int myAtoi(String s) {
         int ans=0;
+        s=s.trim();
+        char ch[]=s.toCharArray();
         boolean isNegative=false;
 
-        s=s.trim();
-        if(s.length()==0) {
-            return 0;
-        }
-        char digits[]=s.toCharArray();
-
-        if(digits[0]=='-') {
-            isNegative=true;
-        }
-
-        for(int i=0;i<digits.length;i++) {
-            if(i==0 && (digits[0]=='-' || digits[0]=='+')) {
+        for(int i=0;i<ch.length;i++) {
+            if(i==0 && ch[i]=='-') {
+                isNegative=true;
+                continue;
+            }
+            else if(i==0 && ch[i]=='+') {
                 continue;
             }
 
-            if(Character.isDigit(digits[i])) {
-                if(ans>Integer.MAX_VALUE/10) {
+            if(!Character.isDigit(ch[i])) {
+                break;
+            }
+
+            if(ans>Integer.MAX_VALUE/10) {
+                if(isNegative)
+                    return Integer.MIN_VALUE;
+                else
+                    return Integer.MAX_VALUE;
+            }
+            else if(ans==Integer.MAX_VALUE/10) {
+                if(ch[i]-'0'>7) {
                     if(isNegative)
                         return Integer.MIN_VALUE;
                     else
                         return Integer.MAX_VALUE;
                 }
-                else if(ans==Integer.MAX_VALUE/10) {
-                    if(digits[i]-'0'>7) {
-                        if(isNegative)
-                            return Integer.MIN_VALUE;
-                        else
-                            return Integer.MAX_VALUE;
-                    }
-                }
-                ans=ans*10+(digits[i]-'0');
             }
-            else {
-                break;
-            }
+            
+            ans=ans*10+(ch[i]-'0');
         }
-        
-        if(isNegative) {
+
+        if(isNegative) 
             return ans*-1;
-        }
-        else {
+        else
             return ans;
-        }
     }
 }

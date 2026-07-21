7bf0ee1b4396fad7711316400ff117daea32e042
Piyush Sohanda <psohanda007@gmail.com>
2026-07-21 09:17:43 +0530

    Time: 13 ms (25.91%), Space: 50 MB (26.58%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -4,25 +4,21 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> ans=new ArrayList<>();
         HashMap<String,ArrayList<String>> map=new HashMap<>();
-        HashMap<Character,Integer> freq=new HashMap<>();
-
+        
         for(String str:strs) {
+            int freq[]=new int[26];
+
             for(char ch:str.toCharArray()) {
-                if(freq.containsKey(ch)) {
-                    freq.put(ch,freq.get(ch)+1);
-                }
-                else {
-                    freq.put(ch,1);
-                }
+                freq[ch-'a']+=1;
             }
 
             StringBuilder temp=new StringBuilder();
-            Character letters[]=freq.keySet().toArray(new Character[0]);
-            Arrays.sort(letters);
-
-            for(char ch:letters) {
-                temp.append(ch);
-                temp.append(freq.get(ch));
+            for(int i=0;i<26;i++) {
+                if(freq[i]!=0) {
+                    temp.append('a'+i);
+                    temp.append(freq[i]);
+                }
+                
             }
             String t=temp.toString();
 
@@ -33,7 +29,6 @@ class Solution {
                 map.put(t, new ArrayList<>());
                 map.get(t).add(str);
             }
-            freq.clear();
         }
 
         for(Map.Entry<String,ArrayList<String>> e:map.entrySet()) {

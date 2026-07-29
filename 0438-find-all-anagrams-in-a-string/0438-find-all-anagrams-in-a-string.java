95574137a0ecb1598bd524d1b22a889d5885c2df
Piyush Sohanda <psohanda007@gmail.com>
2026-07-29 17:18:34 +0530

    Time: 5 ms (98.53%), Space: 47.1 MB (36.68%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -3,28 +3,37 @@ import java.util.*;
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         ArrayList<Integer> ans=new ArrayList<>();
-        int a[]=new int[26];
+        int pmap[]=new int[26];
+        int smap[]=new int[26];
+
+        if(s.length()<p.length()) {
+            return ans;
+        }
 
         for(char ch:p.toCharArray()) {
-            a[ch-'a']++;
+            pmap[ch-'a']++;
+        }
+
+        for(int i=0;i<p.length();i++) {
+            smap[s.charAt(i)-'a']++;
         }
 
         for(int i=0;i<s.length()-p.length()+1;i++) {
             boolean isAna=true;
-            int b[]=new int[26];
-            String sub=s.substring(i,i+p.length());
-
-            for(char ch:sub.toCharArray()) {
-                b[ch-'a']++;
-            }
-
             for(int j=0;j<26;j++) {
-                if(a[j]!=b[j])
+                if(pmap[j]!=smap[j]) {
                     isAna=false;
+                    break;
+                }
             }
 
             if(isAna)
                 ans.add(i);
+
+            if(i+p.length()!=s.length()) {
+                smap[s.charAt(i)-'a']--;
+                smap[s.charAt(i+p.length())-'a']++;
+            }
         }
 
         return ans;

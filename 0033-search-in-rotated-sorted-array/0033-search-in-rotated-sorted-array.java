eea33de8a9bce29732101f97ab2a48bb75eb30e6
Piyush Sohanda <psohanda007@gmail.com>
2026-08-10 20:14:19 +0530

    Time: 0 ms (100%), Space: 43.7 MB (65.82%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,38 +1,31 @@
 class Solution {
-    public int bs(int[] nums, int start, int end, int target) {
+    public int search(int[] nums, int target) {
+        int start=0,end=nums.length-1;
+
         while(start<=end) {
             int mid=start+(end-start)/2;
 
-            if(nums[mid]>target) {
-                end=mid-1;
+            if(nums[mid]==target) {
+                return mid;
             }
-            else if(nums[mid]<target) {
-                start=mid+1;
+
+            if(nums[mid]>=nums[start]) {
+                if(nums[start]<=target && target<nums[mid]) {
+                    end=mid-1;
+                }
+                else {
+                    start=mid+1;
+                }
             }
             else {
-                return mid;
+                if(nums[mid]<target && target<=nums[end]) {
+                    start=mid+1;
+                }
+                else {
+                    end=mid-1;
+                }
             }
         }
         return -1;
-    }
-
-    public int search(int[] nums, int target) {
-        int start=0,end=nums.length-1,pivot=0;
-
-        for(int i=0;i<nums.length-1;i++) {
-            if(nums[i]>nums[i+1]) {
-                pivot=i+1;
-            }
-        }
-        
-        int pass1=bs(nums,start,pivot,target);
-        int pass2=bs(nums,pivot,end,target);
-
-        if(pass1==-1 && pass2==-1)
-            return -1;
-        else if(pass1!=-1)
-            return pass1;
-        else
-            return pass2;
     }
 }

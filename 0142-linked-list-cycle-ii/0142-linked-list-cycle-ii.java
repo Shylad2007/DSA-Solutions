92d96b3f29021debd8b4354310b5bb5ba60d154f
Piyush Sohanda <psohanda007@gmail.com>
2026-08-29 13:36:36 +0530

    Time: 0 ms (100%), Space: 46.9 MB (13.26%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow=head,fast=head;
+
+        while(fast!=null && fast.next!=null) {
+            slow=slow.next;
+            fast=fast.next.next;
+
+            if(slow==fast) {
+                ListNode i=head;
+                ListNode j=slow;
+
+                while(i!=j) {
+                    i=i.next;
+                    j=j.next;
+                }
+                return i;
+            }
+        }
+
+        return null;
+    }
+}

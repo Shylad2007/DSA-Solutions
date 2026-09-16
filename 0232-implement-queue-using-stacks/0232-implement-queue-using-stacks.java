ef5ac88b5a389326a79a05a6f80c444f6d3e3b2f
Piyush Sohanda <psohanda007@gmail.com>
2026-09-16 08:50:41 +0530

    Time: 0 ms (100%), Space: 42.1 MB (99.48%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -13,35 +13,27 @@ class MyQueue {
     }
     
     public int pop() {
-        while(!st1.isEmpty()) {
-            st2.push(st1.pop());
+        if(st2.isEmpty()) {
+            while(!st1.isEmpty()) {
+                st2.push(st1.pop());
+            }
         }
 
-        int ans=st2.pop();
-
-        while(!st2.isEmpty()) {
-            st1.push(st2.pop());
-        }
-
-        return ans;
+        return st2.pop();
     }
     
     public int peek() {
-        while(!st1.isEmpty()) {
-            st2.push(st1.pop());
+        if(st2.isEmpty()) {
+            while(!st1.isEmpty()) {
+                st2.push(st1.pop());
+            }
         }
 
-        int ans=st2.peek();
-
-        while(!st2.isEmpty()) {
-            st1.push(st2.pop());
-        }
-
-        return ans;
+        return st2.peek();
     }
     
     public boolean empty() {
-        return st1.isEmpty();
+        return st1.isEmpty() && st2.isEmpty();
     }
 }
 
